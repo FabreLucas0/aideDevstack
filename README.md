c99fdf6f8ea75962c0f0223a0ef01d84f8ab3b32
lucas <lucas.fabre@etu.univ-amu.fr>
2023-07-25 12:36:56 +0200

    update script+README

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Pour l'os il faut rocky linux 9 voici le lien https://rockylinux.org/fr/download
 Pour ce script je me suis basé sur le site https://docs.openstack.org/devstack/latest/guides/multinode-lab.html  
 Ce script marche uniquement sur des os du type Fedora Centos/RHEL.  
 Ce script fonctionne avec le fichier de config fourni.  
-Le fichier config précise comment choisire la fonction du noeud (controller,compute)
+Le fichier config précise comment choisire la fonction du noeud (controller,compute)  
 Ce script doit etre executé avec les droits sudo  
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # ça permet de pouvoir utilisé les variable dans le fichier config
-source 'config.conf'
+source "config.conf"
 
 # dans la premiere partie le script vas configurer le réseau du noeud
 touch /etc/sysconfig/network-scripts/ifcfg-eth0
@@ -8,7 +8,7 @@ echo "IPADDR=$IPADDR" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "NETMASK=$NETMASK" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "GATEWAY=$GATEWAY" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 
-# le script vas créer l'utilisateur stack
+# le script vas créer l"utilisateur stack
 useradd -s /bin/bash -d /opt/stack -m stack
 chmod +x /opt/stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
@@ -27,44 +27,44 @@ git branch stable/2023.1
 # le script vas utilisé le compte stack créer plustot
 sudo -u stack -i
 
-# si dans le fichier de config on met que le noeud sur lequel on utilise le script est 'controller' alors on fais l'installation comme sur le site d'openstack
-if [ $NODE == 'controller' ]
+# si dans le fichier de config on met que le noeud sur lequel on utilise le script est "controller" alors on fais l"installation comme sur le site d"openstack
+if [ $NODE == "controller" ]
 then
 touch local.conf
-echo '[[local|localrc]]' > local.conf
-echo 'HOST_IP=$HOST_IP' >> local.conf
-echo 'FIXED_RANGE=$FIXED_RANGE' >> local.conf
-echo 'FLOATING_RANGE=$FLOATING_RANGE=' >> local.conf
-echo 'LOGFILE=$LOGFILE' >> local.conf
-echo 'ADMIN_PASSWORD=$ADMIN_PASSWORD' >> local.conf
-echo 'DATABASE_PASSWORD=$DATABASE_PASSWORD' >> local.conf
-echo 'RABBIT_PASSWORD=$RABBIT_PASSWORD' >> local.conf
-echo 'SERVICE_PASSWORD=SERVICE_PASSWORD' >> local.conf
+echo "[[local|localrc]]" > local.conf
+echo "HOST_IP=$HOST_IP" >> local.conf
+echo "FIXED_RANGE=$FIXED_RANGE" >> local.conf
+echo "FLOATING_RANGE=$FLOATING_RANGE=" >> local.conf
+echo "LOGFILE=$LOGFILE" >> local.conf
+echo "ADMIN_PASSWORD=$ADMIN_PASSWORD" >> local.conf
+echo "DATABASE_PASSWORD=$DATABASE_PASSWORD" >> local.conf
+echo "RABBIT_PASSWORD=$RABBIT_PASSWORD" >> local.conf
+echo "SERVICE_PASSWORD=$SERVICE_PASSWORD" >> local.conf
 ./stack.sh
 fi
 
-# si dans le fichier de config on met que le noeud sur lequel on utilise le script est 'compute' alors on fais l'installation comme sur le site d'openstack
-if [ $NODE == 'compute']
+# si dans le fichier de config on met que le noeud sur lequel on utilise le script est "compute" alors on fais l"installation comme sur le site d"openstack
+if [ $NODE == "compute"]
 then
 touch local.conf
-echo '[[local|localrc]]' > local.conf
-echo 'HOST_IP=$HOST_IP' >> local.conf
-echo 'FIXED_RANGE=$FIXED_RANGE' >> local.conf
-echo 'FLOATING_RANGE=$FLOATING_RANGE=' >> local.conf
-echo 'LOGFILE=$LOGFILE' >> local.conf
-echo 'ADMIN_PASSWORD=$ADMIN_PASSWORD' >> local.conf
-echo 'DATABASE_PASSWORD=$DATABASE_PASSWORD' >> local.conf
-echo 'RABBIT_PASSWORD=$RABBIT_PASSWORD' >> local.conf
-echo 'SERVICE_PASSWORD=SERVICE_PASSWORD' >> local.conf
-echo 'DATABASE_TYPE=$DATABASE_TYPE' >> local.conf
-echo 'SERVICE_HOST=$SERVICE_HOST' >> local.conf
-echo 'MYSQL_HOST=$SMYSQL_HOST' >> local.conf
-echo 'RABBIT_HOST=$RABBIT_HOST' >> local.conf
-echo 'GLANCE_HOSTPORT=$GLANCE_HOSTPORT' >> local.conf
-echo 'ENABLED_SERVICES=$ENABLED_SERVICES' >> local.conf
-echo 'NOVA_VNC_ENABLED=$NOVA_VNC_ENABLED' >> local.conf
-echo 'NOVNCPROXY_URL=$NOVNCPROXY_URL' >> local.conf
-echo 'VNCSERVER_LISTEN=$VNCSERVER_LISTEN' >> local.conf
-echo 'VNCSERVER_PROXYCLIENT_ADDRESS=$VNCSERVER_PROXYCLIENT_ADDRESS' >> local.conf
+echo "[[local|localrc]]" > local.conf
+echo "HOST_IP=$HOST_IP" >> local.conf
+echo "FIXED_RANGE=$FIXED_RANGE" >> local.conf
+echo "FLOATING_RANGE=$FLOATING_RANGE=" >> local.conf
+echo "LOGFILE=$LOGFILE" >> local.conf
+echo "ADMIN_PASSWORD=$ADMIN_PASSWORD" >> local.conf
+echo "DATABASE_PASSWORD=$DATABASE_PASSWORD" >> local.conf
+echo "RABBIT_PASSWORD=$RABBIT_PASSWORD" >> local.conf
+echo "SERVICE_PASSWORD=$SERVICE_PASSWORD" >> local.conf
+echo "DATABASE_TYPE=$DATABASE_TYPE" >> local.conf
+echo "SERVICE_HOST=$SERVICE_HOST" >> local.conf
+echo "MYSQL_HOST=$SMYSQL_HOST" >> local.conf
+echo "RABBIT_HOST=$RABBIT_HOST" >> local.conf
+echo "GLANCE_HOSTPORT=$GLANCE_HOSTPORT" >> local.conf
+echo "ENABLED_SERVICES=$ENABLED_SERVICES" >> local.conf
+echo "NOVA_VNC_ENABLED=$NOVA_VNC_ENABLED" >> local.conf
+echo "NOVNCPROXY_URL=$NOVNCPROXY_URL" >> local.conf
+echo "VNCSERVER_LISTEN=$VNCSERVER_LISTEN" >> local.conf
+echo "VNCSERVER_PROXYCLIENT_ADDRESS=$VNCSERVER_PROXYCLIENT_ADDRESS" >> local.conf
 ./stack.sh
 fi
